49f3e8e259459c7be338db3b21f2509ec0951b4c
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-05-11 21:45:33 +0600

    get user

--- index.js
+++ index.js
@@ -28,9 +28,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("userDB");
+    const userCollection = database.collection("users");
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("new user", user);
+      const result = await userCollection.insertOne(user);
+      res.json(result);
     });
 
     // Send a ping to confirm a successful connection
