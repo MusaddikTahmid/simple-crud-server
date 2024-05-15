a36995c8cda6be1a1a841ccfd389402d7095d190
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-05-15 23:51:56 +0600

    somthing new

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ async function run() {
 
     app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
     });
 
     app.post("/users", async (req, res) => {
